aee7b3ee2b7a316a65ba177f424cf07c605ec59c
Khoi Vu <kairyvd167@gmail.com>
2025-04-04 19:29:10 -0400

    add user model

---
+++ flaskr/models/user.py
@@ -0,0 +1,85 @@
+from flaskr import db
+
+class User(db.Model):
+    __tablename__ = 'user'
+    
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    password = db.Column(db.String(128), nullable=False)
+    account_type = db.Column(db.String(20), nullable=False)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, server_default=db.func.now(), onupdate=db.func.now())
+
+    def __repr__(self):
+        return f'<User {self.username}>'
+
+class SuperUser(db.Model):
+    __tablename__ = 'super_user'
+    
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), primary_key=True, nullable=False)
+    user = db.relationship('User', backref=db.backref('super_user', uselist=False))
+
+    def __repr__(self):
+        return f'<SuperUser {self.user_id}>'
+    
+class Doctor(db.Model):
+    __tablename__ = 'doctor'
+    
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), primary_key=True, nullable=False)
+    first_name = db.Column(db.String(80), nullable=False)
+    last_name = db.Column(db.String(80), nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    phone = db.Column(db.String(20), nullable=False)
+    specialization = db.Column(db.String(80), nullable=False)
+    bio = db.Column(db.Text, nullable=True)
+    fee = db.Column(db.Float, nullable=False)
+    profile_picture = db.Column(db.String(120), nullable=True)
+    dob = db.Column(db.Date, nullable=False)
+    license_id = db.Column(db.String(80), unique=True, nullable=False)
+    
+    user = db.relationship('User', backref=db.backref('doctor', uselist=False))
+
+    def __repr__(self):
+        return f'<Doctor {self.first_name} {self.last_name}>'
+    
+class Pharmacy(db.Model):
+    __tablename__ = 'pharmacy'
+    
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), primary_key=True, nullable=False)
+    pharmacy_name = db.Column(db.String(80), nullable=False)
+    phone = db.Column(db.String(20), nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    hours = db.Column(db.String(80), nullable=False)
+    zipcode = db.Column(db.String(20), nullable=False)
+    
+    user = db.relationship('User', backref=db.backref('pharmacy', uselist=False))
+
+    def __repr__(self):
+        return f'<Pharmacy {self.name}>'
+
+
+class Patient(db.Model):
+    __tablename__ = 'patient'
+    
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), primary_key=True, nullable=False)
+    first_name = db.Column(db.String(80), nullable=False)
+    last_name = db.Column(db.String(80), nullable=False)
+    dob = db.Column(db.Date, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    phone = db.Column(db.String(20), nullable=False)
+    doctor_id = db.Column(db.Integer, db.ForeignKey('doctor.user_id'), nullable=True)
+    pharmacy_id = db.Column(db.Integer, db.ForeignKey('pharmacy.user_id'), nullable=True)
+    
+    user = db.relationship('User', backref=db.backref('patient', uselist=False))
+    doctor = db.relationship('Doctor', backref=db.backref('patients', lazy=True))
+    pharmacy = db.relationship('Pharmacy', backref=db.backref('patients', lazy=True))
+
+    def __init__(self, first_name, last_name, dob, email, phone):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.dob = dob
+        self.email = email
+        self.phone = phone
+
+    def __repr__(self):
+        return f'<Patient {self.first_name} {self.last_name}>'
