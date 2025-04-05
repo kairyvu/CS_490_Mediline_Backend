04bb913ca3d22e7ad7fb8cfaf40450ec0d083258
Khoi Vu <kairyvd167@gmail.com>
2025-04-05 10:10:16 -0400

    handle circular import

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,7 +1,24 @@
 from flask import Flask
-from flaskr.routes import register_routes
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__, instance_relative_config=True)
+    username = os.getenv("MYSQL_USER", "root")
+    password = os.getenv("MYSQL_PASSWORD", "")
+    host = os.getenv("MYSQL_HOST", "localhost")
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql+pymysql://{username}:{password}@{host}/doctor_patient_system'
+
+    db.init_app(app)
+    migrate = Migrate(app, db)
+    
+    from flaskr.routes import register_routes
     register_routes(app)
+
     return app
