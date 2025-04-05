e659eef33e45da1ec724843c291e1777a9ad4c89
Khoi Vu <kairyvd167@gmail.com>
2025-04-04 20:38:08 -0400

    add report and exercise models

---
+++ flaskr/models/report.py
@@ -0,0 +1,44 @@
+from flaskr import db
+import enum
+
+class ReportType(enum.Enum):
+    DAILY = 'daily'
+    WEEKLY = 'weekly'
+    MONTHLY = 'monthly'
+
+class Report(db.Model):
+    __tablename__ = 'report'
+
+    report_id = db.Column(db.Integer, primary_key=True)
+    type = db.Column(db.Enum(ReportType), nullable=False)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+
+    def __init__(self, type):
+        self.type = type
+    
+class PatientReport(db.Model):
+    __tablename__ = 'patient_report'
+
+    patient_report_id = db.Column(db.Integer, primary_key=True)
+    report_id = db.Column(db.Integer, db.ForeignKey('report.report_id'), nullable=False, ondelete='CASCADE')
+    patient_id = db.Column(db.Integer, db.ForeignKey('patient.user_id'), nullable=False, ondelete='CASCADE')
+    doctor_id = db.Column(db.Integer, db.ForeignKey('doctor.user_id'), nullable=False, ondelete='CASCADE')
+    height = db.Column(db.Float, nullable=True)
+    weight = db.Column(db.Float, nullable=True)
+    calories_intake = db.Column(db.Integer, nullable=True)
+    hours_of_exercise = db.Column(db.Integer, nullable=True)
+    hours_of_sleep = db.Column(db.Integer, nullable=True)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+
+    def __init__(self, report_id, patient_id, doctor_id, height=None, weight=None, calories_intake=None, hours_of_exercise=None, hours_of_sleep=None):
+        self.report_id = report_id
+        self.patient_id = patient_id
+        self.doctor_id = doctor_id
+        self.height = height
+        self.weight = weight
+        self.calories_intake = calories_intake
+        self.hours_of_exercise = hours_of_exercise
+        self.hours_of_sleep = hours_of_sleep
+
+    def __repr__(self):
+        return f'<Patientreport {self.report_id} {self.patient_id}>'
