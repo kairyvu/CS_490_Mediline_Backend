21792ddb18e2cbaeed01900a48999e61a2df91c5
Ben Macaro <bjm57@njit.edu>
2025-04-10 17:10:12 -0400

    slightly changed flaskr.__init__ for tests

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,20 +1,24 @@
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
 import os
 from dotenv import load_dotenv
+
+from flask import Flask
+#from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 from flaskr.cli import register_commands
-from flaskr.extensions import db
 
 load_dotenv()
 
-def create_app():
+def create_app(test_db_uri: str|None=None):
     app = Flask(__name__, instance_relative_config=True)
+
+    # From .env load mysql authentication
     username = os.getenv("MYSQL_USER", "root")
     password = os.getenv("MYSQL_PASSWORD", "")
     host = os.getenv("MYSQL_HOST", "localhost")
-    app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql+pymysql://{username}:{password}@{host}/doctor_patient_system'
+    schema = os.getenv("MYSQL_DATABASE")
+    app.config['SQLALCHEMY_DATABASE_URI'] = test_db_uri or f'mysql+pymysql://{username}:{password}@{host}/{schema}'
 
+    from flaskr.extensions import db
     db.init_app(app)
     migrate = Migrate(app, db)
     
