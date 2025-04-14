590e944eba5003ee013a5d604a5ea5c29553f38f
Khoi Vu <kairyvd167@gmail.com>
2025-04-14 19:15:32 -0400

    update phone generator

--- flaskr/services/__init__.py
+++ flaskr/services/__init__.py
@@ -5,6 +5,7 @@ from .report_service import get_patient_report_result, add_patient_report
 from .prescription_service import get_medications_by_prescription, get_prescriptions
 from .chat_service import get_current_chat
 from .social_media_service import get_all_posts, get_comments_of_post
+from .auth_service import user_id_credentials
 
 __all__ = ['get_exercises', 'get_all_patient_exercise', 'add_patient_exercise', 'update_patient_exercise',
            'get_invoices_by_user',
@@ -12,4 +13,5 @@ __all__ = ['get_exercises', 'get_all_patient_exercise', 'add_patient_exercise', 
            'get_patient_report_result', 'add_patient_report',
            'get_medications_by_prescription', 'get_prescriptions',
            'get_current_chat',
-           'get_all_posts', 'get_comments_of_post']
+           'get_all_posts', 'get_comments_of_post',
+           'user_id_credentials']
