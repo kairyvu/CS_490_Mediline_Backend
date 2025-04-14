3b8c1be88c077d0766aba2f9cc47c09b04c9994e
Ben Macaro <bjm57@njit.edu>
2025-04-14 14:41:38 -0400

    update imports in services/__init__.py

--- flaskr/services/__init__.py
+++ flaskr/services/__init__.py
@@ -1,3 +1,4 @@
+from .auth_service import user_id_credentials
 from .exercise_service import get_exercises, get_all_patient_exercise, add_patient_exercise, update_patient_exercise
 from .payment_service import get_invoices_by_user
 from .appointment_service import get_upcoming_appointments, add_appointment, update_appointment, get_appointment
@@ -6,7 +7,8 @@ from .prescription_service import get_medications_by_prescription, get_prescript
 from .chat_service import get_current_chat
 from .social_media_service import get_all_posts, get_comments_of_post
 
-__all__ = ['get_exercises', 'get_all_patient_exercise', 'add_patient_exercise', 'update_patient_exercise',
+__all__ = ['user_id_credentials',
+           'get_exercises', 'get_all_patient_exercise', 'add_patient_exercise', 'update_patient_exercise',
            'get_invoices_by_user',
            'get_upcoming_appointments', 'add_appointment', 'update_appointment', 'get_appointment',
            'get_patient_report_result', 'add_patient_report',
