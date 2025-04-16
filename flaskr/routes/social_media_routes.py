18e0a4059b03149fb71b84081feb177737b2f2d3
BM <benjmacaro@gmail.com>
2025-04-16 00:32:59 +0000

    documentation for all routes

--- flaskr/routes/social_media_routes.py
+++ flaskr/routes/social_media_routes.py
@@ -1,9 +1,11 @@
 from flaskr.services import get_comments_of_post, get_all_posts
 from flask import Blueprint, jsonify, request
+from flasgger import swag_from
 
 social_media_bp = Blueprint('social_media', __name__)
 
 @social_media_bp.route('/', methods=['GET'])
+@swag_from('../docs/social_media_routes/get_posts.yml')
 def get_posts():
     try:
         posts = get_all_posts()
@@ -12,6 +14,7 @@ def get_posts():
         return jsonify({"error": str(e)}), 500
 
 @social_media_bp.route('/<int:post_id>/comments', methods=['GET'])
+@swag_from('../docs/social_media_routes/get_post_comments.yml')
 def get_post_comments(post_id):
     sort_by = request.args.get('sort_by', 'created_at')
     order = request.args.get('order', 'asc')
