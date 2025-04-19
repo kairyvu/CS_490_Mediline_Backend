8ef13d90677ae0a5265a76bcfacac8e20522f73f
Ben Macaro <b.macaro0325@gmail.com>
2025-04-19 13:31:44 -0400

    Update __init__.py

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -7,15 +7,15 @@ load_dotenv()
 
 def create_app():
     app = Flask(__name__, instance_relative_config=True)
-    username = os.getenv("DB_USER") or os.getenv("MYSQL_USER")
-    password = os.getenv("DB_PASS") or os.getenv("MYSQL_PASSWORD")
-    host = os.getenv("INSTANCE_HOST") or os.getenv("MYSQL_HOST")
-    port = os.getenv("DB_PORT", "3306")
-    database = os.getenv("DB_NAME", "doctor_patient_system")
 
+    database = os.getenv("DB_NAME", "doctor_patient_system")
     connection_string = 'mysql+pymysql://'
     if os.environ.get('FLASK_ENV') == 'development':
         # When running app on local machine
+        username = os.getenv("DB_USER") or os.getenv("MYSQL_USER", "root")
+        password = os.getenv("DB_PASS") or os.getenv("MYSQL_PASSWORD", "")
+        host = os.getenv("INSTANCE_HOST") or os.getenv("MYSQL_HOST", "localhost")
+        port = os.getenv("DB_PORT", "3306")
         connection_string += f'{username}:{password}@{host}:{port}/{database}'
         app.config['SQLALCHEMY_DATABASE_URI'] = connection_string
     else:
