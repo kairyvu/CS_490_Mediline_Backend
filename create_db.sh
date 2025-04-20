8d6a5a6cdaa1f623d7fa86a2abd052e33b20fe78
Ben Macaro <bjm57@njit.edu>
2025-04-20 14:09:56 -0400

    update create_db

--- create_db.sh
+++ create_db.sh
@@ -2,7 +2,8 @@
 ## Script to create database in mysql
 ## Should be in project root and uses .env
 set -ex
-DEV_ENV=.env
+WORK_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+DEV_ENV=${WORK_DIR}/.env
 cat <<-EOF > tmp_create.sql
 DROP SCHEMA IF EXISTS ${MYSQL_DATABASE};
 CREATE SCHEMA ${MYSQL_DATABASE} DEFAULT CHARACTER SET utf8mb4;
@@ -16,9 +17,11 @@ if [[ -f "$DEV_ENV" ]]; then
 
     rm tmp_create.sql
 else
+    rm tmp_create.sql
     echo "upgrading DB"
     MIGRATIONS=migrations/
     if [[ ! -d $"MIGRATIONS" ]]; then
+        echo "migrations directory not found"
         flask db init
     fi
     flask db migrate
