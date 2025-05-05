6146af508b6de8d879530875a3a2385e11a17c1f
Ben Macaro <b.macaro0325@gmail.com>
2025-05-05 11:45:11 -0400

    foreign key fix

--- flaskr/models/request.py
+++ flaskr/models/request.py
@@ -4,8 +4,8 @@ class PatientRequest(db.Model):
     __tablename__ = 'patient_request'
 
     request_id = db.Column(db.Integer, primary_key=True)
-    doctor_id = db.Column(db.Integer, db.ForeignKey('doctor'), nullable=False)
-    patient_id = db.Column(db.Integer, db.ForeignKey('patient'), nullable=False)
+    doctor_id = db.Column(db.Integer, db.ForeignKey('doctor.user_id'), nullable=False)
+    patient_id = db.Column(db.Integer, db.ForeignKey('patient.user_id'), nullable=False)
     created_at = db.Column(db.DateTime, nullable=False, default=db.func.now())
 
     patient = db.relationship('Patient', backref=db.backref('requests', lazy=True))
