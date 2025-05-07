319ef0700f48f12403fde4f06b3dd3dd5ceb962e
David <dsa38@njit.edu>
2025-05-07 18:54:12 -0400

    Doctor's List of Patients (Details) plus count

--- flaskr/services/__init__.py
+++ flaskr/services/__init__.py
@@ -13,7 +13,7 @@ from .prescription_service import get_medications_by_prescription, get_prescript
 from .chat_service import get_current_chat, add_message
 from .social_media_service import get_all_posts, get_comments_of_post, delete_comment, delete_post, update_comment, update_post, create_comment, create_post
 from .doctor_service import all_doctors, doctor_details, upcoming_appointments_count, pending_appointments_count,\
-    doctor_patients_count, todays_patient, doctor_rating_detail, last_completed_appointment, doctor_general_discussion, \
+    doctor_patients_count_and_list, todays_patient, doctor_rating_detail, last_completed_appointment, doctor_general_discussion, \
     new_appointments_request, update_doctor, assign_survey
 from .pharmacy_service import get_all_pharmacy_patients, add_pt_rx, get_pharmacy_info, validate_rx
 from .patient_service import patient_info, update_patient, update_primary_pharmacy, update_doctor_by_patient_id
@@ -34,7 +34,7 @@ __all__ = [
     'get_prescription_count_by_pharmacy', 'get_pharmacy_medications_inventory', 'get_medications_history_by_patient', 'update_prescription_status',
     'get_current_chat', 'add_message', 
     'get_all_posts', 'get_comments_of_post', 'delete_comment', 'delete_post', 'update_comment', 'update_post', 'create_comment', 'create_post',
-    'all_doctors', 'doctor_details', 'upcoming_appointments_count', 'pending_appointments_count', 'doctor_patients_count', 'todays_patient', 'doctor_rating_detail', 'last_completed_appointment', 'doctor_general_discussion', 'get_all_pharmacy_patients', 'get_pharmacy_info', 'add_pt_rx', 'validate_rx',
+    'all_doctors', 'doctor_details', 'upcoming_appointments_count', 'pending_appointments_count', 'doctor_patients_count_and_list', 'todays_patient', 'doctor_rating_detail', 'last_completed_appointment', 'doctor_general_discussion', 'get_all_pharmacy_patients', 'get_pharmacy_info', 'add_pt_rx', 'validate_rx',
     'patient_info', 'update_patient','patient_medical_history', 'create_medical_record', 'update_primary_pharmacy', 'update_doctor_by_patient_id',
     'add_user', 'new_appointments_request', 'update_doctor', 'assign_survey',
     'medication_info',
